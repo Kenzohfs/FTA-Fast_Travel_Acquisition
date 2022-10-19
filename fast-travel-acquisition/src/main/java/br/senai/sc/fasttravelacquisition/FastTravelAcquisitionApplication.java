9c91bd9d9364c6c857e1d14705c3d6f6064490c1
Kenzohfs <kenzo_sato@estudante.sc.senai.br>
2022-10-19 19:46:36 -0300

    19/10 - 19:46
    created maven project

---
+++ fast-travel-acquisition/src/main/java/br/senai/sc/fasttravelacquisition/FastTravelAcquisitionApplication.java
@@ -0,0 +1,13 @@
+package br.senai.sc.fasttravelacquisition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastTravelAcquisitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastTravelAcquisitionApplication.class, args);
+	}
+
+}
---
+++ fast-travel-acquisition/src/test/java/br/senai/sc/fasttravelacquisition/FastTravelAcquisitionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.sc.fasttravelacquisition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastTravelAcquisitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
