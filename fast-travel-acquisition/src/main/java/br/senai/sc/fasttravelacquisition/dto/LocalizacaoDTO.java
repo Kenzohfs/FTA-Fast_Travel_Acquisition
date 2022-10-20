ae3df67d86b4afd6b95f83aee22aff2e14799d77
Matheus Franzener Hohmann <matheus_hohmann@estudante.sc.senai.br>
2022-10-19 21:15:57 -0300

    19/10 - 21:15
    Localizacao nao terminada

--- fast-travel-acquisition/src/main/java/br/senai/sc/fasttravelacquisition/dto/LocalizacaoDTO.java
+++ fast-travel-acquisition/src/main/java/br/senai/sc/fasttravelacquisition/dto/LocalizacaoDTO.java
@@ -1,4 +1,17 @@
 package br.senai.sc.fasttravelacquisition.dto;
 
+import br.senai.sc.fasttravelacquisition.model.entities.TipoLocalizacao;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
 public class LocalizacaoDTO {
+
+    private Long id;
+    private TipoLocalizacao tipo;
+    private Date horario;
+    private Long viagem_id;
+    private Long endereco_id;
+
 }
--- fast-travel-acquisition/src/main/java/br/senai/sc/fasttravelacquisition/model/entities/TipoLocalizacao.java
+++ fast-travel-acquisition/src/main/java/br/senai/sc/fasttravelacquisition/model/entities/TipoLocalizacao.java
@@ -1,7 +1,14 @@
 package br.senai.sc.fasttravelacquisition.model.entities;
 
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public enum TipoLocalizacao {
 
+    PARTIDA("Partida"),
+    DESTINO("Destino"),
+    PARADA("Parada");
+
+    private String tipoLocalizacao;
 
 }
