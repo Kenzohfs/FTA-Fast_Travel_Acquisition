2b79d2f9f5d36272692abab7d19610bf14653a04
Felipe Vieira <felipemielkevieira2@gmail.com>
2022-10-19 21:12:53 -0300

    19/10 - 21:12

--- fast-travel-acquisition/src/main/java/br/senai/sc/fasttravelacquisition/controller/ViagemController.java
+++ fast-travel-acquisition/src/main/java/br/senai/sc/fasttravelacquisition/controller/ViagemController.java
@@ -2,6 +2,7 @@ package br.senai.sc.fasttravelacquisition.controller;
 
 import br.senai.sc.fasttravelacquisition.dto.ViagemDTO;
 import br.senai.sc.fasttravelacquisition.model.entities.Viagem;
+import br.senai.sc.fasttravelacquisition.model.service.VeiculoService;
 import br.senai.sc.fasttravelacquisition.model.service.ViagemService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.BeanUtils;
@@ -20,7 +21,7 @@ public class ViagemController {
 
     private ViagemService viagemService;
 
-    @GetMapping //Todas Viagens
+    @GetMapping
     public ResponseEntity<List<Viagem>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(viagemService.findAll());
     }
@@ -37,13 +38,23 @@ public class ViagemController {
 
     @GetMapping("/{codigo}")
     public ResponseEntity<Object> findByVeiculo(@PathVariable(value = "codigo") String id) {
-        // Fazer Verificação
+
+        VeiculoService service = new VeiculoService();
+        if(!service.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ID do veículo não encontrado!");
+        }
 
         return ResponseEntity.status(HttpStatus.OK).body(viagemService.findByVeiculo(id));
     }
 
     @PostMapping
     public ResponseEntity<Object> save(@RequestBody @Valid ViagemDTO viagemDTO) {
+
+        VeiculoService service = new VeiculoService();
+        if(!service.existsById(viagemDTO.getVeiculo().getCodigo())) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Código do veículo não encontrado!");
+        }
+
         Viagem viagem = new Viagem();
         BeanUtils.copyProperties(viagemDTO, viagem);
         return ResponseEntity.status(HttpStatus.OK).body(viagemService.save(viagem));
@@ -54,6 +65,11 @@ public class ViagemController {
 
         if(!viagemService.existsById(id)) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ID não encontrado!");
+        }
+
+        VeiculoService service = new VeiculoService();
+        if(!service.existsById(viagemDTO.getVeiculo().getCodigo())) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Código do veículo não encontrado!");
         }
 
         Viagem viagem = new Viagem();
