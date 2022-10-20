8e30e480ee865c32f8aecddccbff04707137dbcc
Felipe Vieira <felipemielkevieira2@gmail.com>
2022-10-19 21:51:57 -0300

    19/10 - 21:51

--- fast-travel-acquisition/src/main/java/br/senai/sc/fasttravelacquisition/model/service/VeiculoService.java
+++ fast-travel-acquisition/src/main/java/br/senai/sc/fasttravelacquisition/model/service/VeiculoService.java
@@ -2,12 +2,16 @@ package br.senai.sc.fasttravelacquisition.model.service;
 
 import br.senai.sc.fasttravelacquisition.model.entities.Veiculo;
 import br.senai.sc.fasttravelacquisition.repository.VeiculoRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
+@NoArgsConstructor
 public class VeiculoService {
 
     private VeiculoRepository veiculoRepository;
